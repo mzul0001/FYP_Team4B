0267e5506963251291896b35b0d0d07b24287a64
pandapris <ptha0007@student.monash.edu>
2019-12-30 13:38:32 +0800

    Replaced magic numbers

--- cv2hog_code.py
+++ cv2hog_code.py
@@ -5,7 +5,7 @@ def featureExtraction_HOG(img):
     BGR_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     dim = (width, height) = (240, 240)
     # resize image so hog does not miss cascading part of the image especially the edges
-    img = cv2.resize(img, dsize=dim)
+    BGR_img = cv2.resize(BGR_img, dsize=dim)
 
     # Sobel: discrete differentiation operator
     # edges of an image is detected by obtaining the first derivative
@@ -18,12 +18,12 @@ def featureExtraction_HOG(img):
     # magnitude, direction = cv2.cartToPolar(hx, hy, angleInDegrees=True)
 
     # non-default HOG values
-    winSize = (240, 240)
-    blockSize = (16, 16)
-    blockStride = (8, 8)
+    winSize = (BGR_img.shape[0], BGR_img[1])
     cellSize = (8, 8)
+    blockSize = (cellSize[0]+cellSize[0], cellSize[1]+cellSize[1])
+    blockStride = (cellSize[0], cellSize[1])
     nbins = 9
 
     hog = cv2.HOGDescriptor(winSize, blockSize, blockStride, cellSize, nbins, 1, -1, 0, 0.2, 1, 64)
-    features = hog.compute(img)
+    features = hog.compute(BGR_img)
     return features
