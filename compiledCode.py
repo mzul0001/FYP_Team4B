79a2dbc2262eebca268da822f0e0f1708e998341
pandapris <ptha0007@student.monash.edu>
2020-01-06 19:33:45 +0800

    Added f1-score computation

--- compiledCode.py
+++ compiledCode.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 
-def IP(img, count):
+def ImageProcessing(img, imgName, faceNo):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # Histogram Equalization
     gray = cv2.equalizeHist(gray)
@@ -11,65 +11,111 @@ def IP(img, count):
     gaussian = cv2.GaussianBlur(gray, (9, 9), 10.0)
     gray = cv2.addWeighted(gray, 1.5, gaussian, -0.5, 0, gray)
 
-    faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-    color = (0, 0, 255)
+    # obtain cascade classifiers
+    # FaceAddress ='C:\\Users\\User\\AppData\\Local\\Programs\\Python\\Python37-32\\'
+    # FaceAddress +='Lib\\site-packages\\cv2\\data\\haarcascade_frontalface_default.xml'
+    # FaceCascade = cv2.CascadeClassifier(FaceAddress)
 
-    # scale factor -- how much a given image is shrunk to be processed
-    # minNeighbours -- If the value is bigger, it detects less objects but less misdetections. If smaller,
-    # it detects more objects but more misdetections.
-    # faces = faceCascade.detectMultiScale(image = gray, scaleFactor = 1.25,minNeighbors= 2)
-    faces = faceCascade.detectMultiScale(image=gray, scaleFactor=1.3, minNeighbors=6)
+    # EyeAddress ='C:\\Users\\User\\AppData\\Local\\Programs\\Python\\Python37-32\\'
+    # EyeAddress +='Lib\\site-packages\\cv2\\data\\haarcascade_eye.xml'
+    # EyeCascade = cv2.CascadeClassifier(EyeAddress)
+    #
+    # MouthAddress ='C:\\Users\\User\\AppData\\Local\\Programs\\Python\\Python37-32\\'
+    # MouthAddress +='Lib\\site-packages\\cv2\\data\\haarcascade_mcs_mouth.xml'
+    # MouthCascade = cv2.CascadeClassifier(MouthAddress)
+
+    cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+    color = (0, 0, 255)
+    # color2 = (0, 255, 0)
+    # color3 = (0, 0, 0)
+
+    # scale factor -- how much the image size is reduced at each image scale
+    # minNeighbours -- how many neighbors each candidate rectangle should have to retain it
+    #               -- If the value is bigger, it detects less objects but less misdetections. If smaller,
+    #               it detects more objects but more misdetections.
+    # For each resulting detection, `levelWeights` will then contain the certainty of classification at the final stage.
+    faces, _, levelWeights = cascade.detectMultiScale3(image=gray, scaleFactor=1.3, minNeighbors=6,
+                                                       outputRejectLevels=True)
 
     # draw a rectangle for each face
     for (x, y, width, height) in faces:
         # image, left up coordinate, right down coordinate, color, thickness
         img = cv2.rectangle(img, (x, y), (x + width, y + height), color, 2)
         # crop image
-        saved = img[y:y + height, x:x + width]
-        count += 1
+        cropped = img[y:y + height, x:x + width]
+        # name of the image
+        imgName += str(faceNo) + '.jpg'
+        # save the image into the outputs file
+        cv2.imwrite("./outputs/" + imgName, cropped)
+        faceNo += 1
+    return img, faceNo
 
-    return img, count
 
-
-def faceDetection(filename):
+def videoProcessing(videoName):
     # read a video file
-    video = cv2.VideoCapture(filename)
-    count = 0  # count is the number of images detected and saved
+    video = cv2.VideoCapture(videoName)
+    # count is the number of images detected and saved
+    count = 0
+    # number of frames per second
+    fps = video.get(cv2.CAP_PROP_FPS)
+    width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    writer = cv2.VideoWriter(filename='test_output.avi', apiPreference=0, fourcc=cv2.VideoWriter_fourcc(*'MJPG'),
+                             fps=fps, frameSize=(width, height))
 
     while video.isOpened():
         # read a frame from video
         ret, frame = video.read()
 
-        if count == 0:
-            height, width, layers = frame.shape
-            size = (width,
-                    height)
-            # out = cv2.VideoWriter(filename='output.mp4', apiPreference=0, fourcc=cv2.VideoWriter_fourcc(*'mp4v'),
-            # fps=15, frameSize=size)
-
         # if there is no next frame, the loop terminates
         if not ret: break
 
-        cv2.waitKey(1)
-        frame, count = IP(frame, count)  # detect a face in an image
+        current_frame = video.get(cv2.CAP_PROP_POS_FRAMES)
+        timestamp = current_frame / fps
+        imgName = str(timestamp) + '-' + str(current_frame) + '-'
+        # detect a face in an image
+        frame, count = ImageProcessing(frame, imgName, count)
+        writer.write(frame)
 
-        # out.write(frame)
-        cv2.imshow('frame', frame)
-
-        # stop its execution by pressing Q-key
         key = cv2.waitKey(1) & 0xFF
         if key == ord('q'): break
 
     # release memory
     video.release()
+    writer.release()
     cv2.destroyAllWindows()
-    # out.release()
     return count
+
+
+import tensorflow as tf
+from sklearn.metrics import f1_score, precision_score, recall_score, classification_report
+
+class ModelMetrics(tf.keras.callbacks.Callback):
+    def on_train_begin(self, logs=None):
+        if logs is None:
+            logs = {}
+        self.precisions = []
+        self.recalls = []
+        self.f1_scores = []
+
+    def on_epoch_end(self, epoch, logs=None):
+        if logs is None:
+            logs = {}
+        y_pred = np.asarray(self.model.predict_classes(self.validation_data[0])).round()
+        y_target = self.validation_data[1]
+        y_f1 = f1_score(y_target, y_pred, average='micro')
+        y_recall = recall_score(y_target, y_pred, average='micro')
+        y_precision = precision_score(y_target, y_pred, average='micro')
+        self.f1_scores.append(y_f1)
+        self.recalls.append(y_recall)
+        self.precisions.append(y_precision)
 
 
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.layers import Dropout
 from keras.models import model_from_json
+import matplotlib.pyplot as plt
 
 
 def predict(features):
@@ -86,24 +132,30 @@ def predict(features):
     label = loadedModel.predict_classes(features)
     return label
 
+
 def trainNN(model, train_features, test_features, train_labels, test_labels):
     print("Training NN...")
+    metrics = ModelMetrics()
     # batch_size and epochs can be experimented to change accuracy
-    trained = model.fit(train_features, train_labels, batch_size=256, epochs=20, verbose=1, validation_data=(
-        test_features, test_labels))
+    trained = model.fit(train_features, train_labels, batch_size=71, epochs=20, verbose=2, validation_data=(
+        test_features, test_labels), callbacks=[metrics])
     print("Evaluating NN...")
     [loss, accuracy] = model.evaluate(test_features, test_labels)
+    print("Loss = {}, accuracy = {}".format(loss, accuracy))
+    predicted = model.predict_classes(test_features)
+    category = ['ANGRY', 'DISGUST', 'FEAR', 'HAPPY', 'NEUTRAL', 'SAD', 'SURPRISE']
+    print(classification_report(test_labels, predicted, target_names=category))
 
-    # check for overfitting/underfitting
-    # plot loss curves
+    # # check for overfitting/underfitting
+    # # plot loss curves
     # plt.plot(trained.history['loss'], 'r')
     # plt.plot(trained.history['val_loss'], 'b')
     # plt.legend(['Training loss', 'Test loss'])
     # plt.xlabel('Epochs')
     # plt.ylabel('Loss')
     # plt.title('Loss Curve')
-    # # plt.show()
-    # print(trained.history.keys())
+    # plt.show()
+    #
     # # plot accuracy curves
     # plt.plot(trained.history['accuracy'], 'r')
     # plt.plot(trained.history['val_accuracy'], 'b')
@@ -115,24 +167,26 @@ def trainNN(model, train_features, test_features, train_labels, test_labels):
 
     # serialize model to JSON
     model_json = model.to_json()
-    with open('model.json', 'w') as json_file:
+    with open('model1.json', 'w') as json_file:
         json_file.write(model_json)
 
     # serialize weights to HDF5
-    model.save_weights('model.h5')
-    print("Saved model")
+    model.save_weights('model1.h5')
+    # print("Saved model")
+
 
 def buildNN(data):
     units = 10
     classes = 7
-    data = data.shape[1]
+    input = data.shape[1]
 
     model = Sequential()
-    for i in range(10):
+    for i in range(units):
         # hidden layers
-        model.add(Dense(units, input_shape=(data,), activation='relu'))
+        model.add(Dense(units, input_shape=(input,), activation='relu'))
+        # model.add(Dropout(0.2))
     # output layer
-    model.add(Dense(classes, input_shape=(data,), activation='softmax'))
+    model.add(Dense(classes, input_shape=(input,), activation='softmax'))
 
     # configure model
     #
@@ -142,10 +196,11 @@ def buildNN(data):
     # - computationally efficient, has little memory requirements
     # - suited for problems that are large in terms of data and/or parameters
     #
-    # categorical cross entropy: - also called multi class log loss - measures the performance of a classification
-    # model where the prediction input is a probability value between 0 and 1
+    # categorical cross entropy:
+    # - also called multi class log loss - measures the performance of a classification model where
+    # the prediction input is a probability value between 0 and 1
 
-    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
     print("Compiled NN")
     return model
 
@@ -156,15 +211,54 @@ def featureExtraction_HOG(img):
     # resize image so hog does not miss cascading part of the image especially the edges
     BGR_img = cv2.resize(BGR_img, dsize=dim)
 
+    # Sobel: discrete differentiation operator
+    # edges of an image is detected by obtaining the first derivative
+    # horizontal edge
+    # hy = cv2.Sobel(img, cv2.CV_32F, 0, 1)
+    # vertical edge
+    # hx = cv2.Sobel(img, cv2.CV_32F, 1, 0)
+
+    # obtain second derivative
+    # magnitude, direction = cv2.cartToPolar(hx, hy, angleInDegrees=True)
+
     # non-default HOG values
-    winSize = (240, 240)
-    blockSize = (16, 16)
-    blockStride = (8, 8)
+    winSize = dim
     cellSize = (8, 8)
+    blockSize = (16, 16)
+    blockStride = cellSize
     nbins = 9
 
     hog = cv2.HOGDescriptor(winSize, blockSize, blockStride, cellSize, nbins, 1, -1, 0, 0.2, 1, 64)
     features = hog.compute(BGR_img)
+
+    # # reference: https://stackoverflow.com/questions/6090399/get-hog-image-features-from-opencv-python
+    # n_cells = (BGR_img.shape[0] // cellSize[0], BGR_img.shape[1] // cellSize[1])
+    # n_inner = (blockSize[0] // cellSize[0], blockSize[1] // cellSize[1])
+    # features = features.reshape(n_cells[1] - n_inner[1] + 1, n_cells[0] - n_inner[0] + 1, n_inner[1], n_inner[0], nbins)
+    # features = features.transpose((1, 0, 2, 3, 4))
+    #
+    # gradients = np.zeros((n_cells[0], n_cells[1], nbins))
+    #
+    # # count cells (border cells appear less often across overlapping groups)
+    # cell_count = np.full((n_cells[0], n_cells[1], 1), 0, dtype=int)
+    #
+    # for off_y in range(n_inner[0]):
+    #     for off_x in range(n_inner[1]):
+    #         gradients[off_y:n_cells[0] - n_inner[0] + off_y + 1,
+    #         off_x:n_cells[1] - n_inner[1] + off_x + 1] += features[:, :, off_y, off_x, :]
+    #         cell_count[off_y:n_cells[0] - n_inner[0] + off_y + 1,
+    #         off_x:n_cells[1] - n_inner[1] + off_x + 1] += 1
+    #
+    # # Average gradients
+    # gradients /= cell_count
+    #
+    # bin = 2  # angle is 360 / nbins * direction
+    # plt.pcolor(gradients[:, :, bin])
+    # plt.gca().invert_yaxis()
+    # plt.gca().set_aspect('equal', adjustable='box')
+    # plt.colorbar()
+    # plt.show()
+
     return features
 
 
@@ -172,20 +266,22 @@ import os
 from keras.utils import np_utils
 
 
-def processDataset():
+def processDataset_jaffe():
     address = './jaffe/'
     dirList = os.listdir(address)
 
     images = []
 
+    print("Reading images...")
     for dir in dirList:
         imgList = os.listdir(address+dir)
         for img in imgList:
             imgRead = cv2.imread(address+dir+'/'+img)
-            images += [cv2.resize(imgRead, (240, 240))]
+            images += [imgRead]
 
     images = np.asarray(images)
 
+    print("Extracting features...")
     data = []
     for i in range(images.shape[0]):
         data += [featureExtraction_HOG(images[0])]
@@ -203,7 +299,7 @@ def processDataset():
     labels[125:155] = 4
     labels[156:187] = 5
     labels[188:] = 6
-    labels = np_utils.to_categorical(labels)
+    # labels = np_utils.to_categorical(labels)
 
     seed = np.arange(data.shape[0])
     np.random.shuffle(seed)
@@ -215,16 +311,95 @@ def processDataset():
     test_labels = labels[0:n]
     train_features = data[n:]
     train_labels = labels[n:]
+    print("Processed images")
 
     model = buildNN(data)
     trainNN(model, train_features, test_features, train_labels, test_labels)
-    # category = ['ANGRY', 'DISGUST', 'FEAR', 'HAPPY', 'NEUTRAL', 'SAD', 'SURPRISE']
+    return
+
+
+def processDataset():
+    address = './face-expression-recognition-dataset/images/'
+    dirList = os.listdir(address)
+
+    train_images = []
+    train_labels = []
+    test_images = []
+    test_labels = []
+
+    print("Reading images...")
+    for dir in dirList:
+        if dir == 'train':
+            count = 0
+            folderList = os.listdir(address+dir)
+            for folder in folderList:
+                imgList = os.listdir(address+dir+'/'+folder)
+                for img in imgList:
+                    imgRead = cv2.imread(address+dir+'/'+folder+'/'+img)
+                    train_images += [cv2.resize(imgRead, (48, 48))]
+                    train_labels += [count]
+                count += 1
+    train_images = np.asarray(train_images)
+
+    for dir in dirList:
+        if dir == 'validation':
+            count = 0
+            folderList = os.listdir(address+dir)
+            for folder in folderList:
+                imgList = os.listdir(address+dir+'/'+folder)
+                for img in imgList:
+                    imgRead = cv2.imread(address+dir+'/'+folder+'/'+img)
+                    test_images += [cv2.resize(imgRead, (48, 48))]
+                    test_labels += [count]
+                count += 1
+    test_images = np.asarray(test_images)
+
+    print("Extracting features...")
+    train_data = []
+    for i in range(train_images.shape[0]):
+        train_data += [featureExtraction_HOG(train_images[0])]
+    train_data = np.asarray(train_data)
+
+    test_data = []
+    for i in range(test_images.shape[0]):
+        test_data += [featureExtraction_HOG(test_images[0])]
+    test_data = np.asarray(test_data)
+
+    dim = np.prod(train_data.shape[1:])
+    train_data = train_data.reshape(train_data.shape[0], dim)
+    train_data = train_data.astype('float32')
+    train_data /= 255
+
+    dim = np.prod(test_data.shape[1:])
+    test_data = test_data.reshape(test_data.shape[0], dim)
+    test_data = test_data.astype('float32')
+    test_data /= 255
+
+    train_labels = np.asarray(train_labels)
+    # train_labels = np_utils.to_categorical(train_labels)
+
+    test_labels = np.asarray(test_labels)
+    # test_labels = np_utils.to_categorical(test_labels)
+
+    seed = np.arange(train_data.shape[0])
+    np.random.shuffle(seed)
+    train_data = train_data[seed]
+    train_labels = train_labels[seed]
+
+    seed = np.arange(test_data.shape[0])
+    np.random.shuffle(seed)
+    test_data = test_data[seed]
+    test_labels = test_labels[seed]
+    print("Processed images")
+
+    model = buildNN(train_data)
+    trainNN(model, train_data, test_data, train_labels, test_labels)
     return
 
 
 def main():
-    faceDetection('sample2a.mp4')
-    # processDataset()
+    # videoProcessing('sample2a.mp4')
+    processDataset_jaffe()
 
 
 if __name__ == '__main__':
