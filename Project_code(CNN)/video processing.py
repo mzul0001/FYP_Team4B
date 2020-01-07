f54bac25a6aa2755843f65b3d73a181bf0681d86
Takayuki-Kimura-git <58288091+Takayuki-Kimura-git@users.noreply.github.com>
2020-01-07 16:39:21 +0800

    improved
    now code doesn't have to save imgs in file

--- Project_code(CNN)/video processing.py
+++ Project_code(CNN)/video processing.py
@@ -2,8 +2,9 @@ import cv2
 import numpy    
 import moviepy.editor as mp
 from CNN import CNN_predict
-
-def ImageProcessing(img,imgname,flag,label):
+import keras
+from keras.models import model_from_json
+def ImageProcessing(img,FaceNo,model):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # Histogram Equalization
@@ -32,32 +33,25 @@ def ImageProcessing(img,imgname,flag,label):
     # For each resulting detection, `levelWeights` will then contain the certainty of classification at the final stage.
     faces, _, levelWeights = FaceCascade.detectMultiScale3(image=gray, scaleFactor=1.3, minNeighbors=6,
                                                        outputRejectLevels=True)
-
-    FaceNo =0
+    labels = []
+    
 
     ##draw a rectangle for each face
     for (x,y,width,height) in faces:
         ##image, left up coordinate, right down coordinate, color, thickness
         img = cv2.rectangle(img,(x,y),(x+width,y+height),color,2)
 
-        #capturing images 
-        if flag ==0:
-            saved = img[y:y+height,x:x+width]
-            #name of the image
-            imgname = imgname+ str(FaceNo)+'.jpg'
-            #save the image into the outputs file
-            cv2.imwrite("./outputs/" +imgname, saved)
-            
-
+        Face = img[y:y+height,x:x+width]
+        Gray = cv2.cvtColor(Face, cv2.COLOR_BGR2GRAY) 
+        resized = cv2.resize(Gray,(28,28))
         
-        #Tagging emotion
-        else:
-            
-            img = cv2.putText(img = img, text = label[FaceNo], org =(x, y),fontFace = cv2.FONT_HERSHEY_SIMPLEX ,fontScale =1.0, color =(255, 255, 255), thickness=1)
-        FaceNo += 1    
+        label=CNN_predict(resized,model)
+        labels.append(label)
+        img = cv2.putText(img = img, text = str(label), org =(x, y),fontFace = cv2.FONT_HERSHEY_SIMPLEX ,fontScale =1.0, color =(255, 255, 255), thickness=1)
+          
             
         
-    return img
+    return img,labels
 
 def VideoProcessing(videoname):
     ##read a video file
@@ -68,8 +62,18 @@ def VideoProcessing(videoname):
     Video_height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
     fps= video.get(cv2.CAP_PROP_FPS)##the number of frames per sec
     ##fourcc is MJPG,DIVX or XVID 
-    writer = cv2.VideoWriter("test_output.avi", cv2.VideoWriter_fourcc(*"MJPG"), fps, (Video_width, Video_height))
+    writer = cv2.VideoWriter("Tagged_video.avi", cv2.VideoWriter_fourcc(*"MJPG"), fps, (Video_width, Video_height))
+    file = open('label.txt','w')
 
+    with open('model.json','r') as json_file:
+        SavedModel = model_from_json(json_file.read())
+    SavedModel.load_weights('model.h5')
+    SavedModel.compile(loss= 'categorical_crossentropy',optimizer ='adam' , metrics=['accuracy'])
+
+    FaceNo =0
+    
+    labels = []
+    
     while video.isOpened():
         #read a fram from video
         ret, frame = video.read()
@@ -85,10 +89,15 @@ def VideoProcessing(videoname):
         timestamp = current_frame / fps
 
         #video cropped for only 15 seconds for testing 
-        if timestamp > 15: break
-        imgname = str(timestamp)+'_' + str(current_frame) +'_'
+        if timestamp > 8: break
+        ##imgname = str(timestamp)+'_' + str(current_frame) +'_'
         
-        frame= ImageProcessing(frame,imgname,0,[]) # detect a face in an image
+        frame,labels = ImageProcessing(frame,FaceNo,SavedModel)
+
+        for emotion in labels:
+            file.write(str(timestamp)+' '+ str(FaceNo) +' ' + str(emotion) +'\n')
+            FaceNo +=1
+        
         writer.write(frame)
         #-----------------
 
@@ -99,6 +108,7 @@ def VideoProcessing(videoname):
         if key == ord('q'): break
      
         #release memory
+    file.close()
     writer.release()
     video.release()
     cv2.destroyAllWindows()
@@ -112,52 +122,57 @@ def VideoProcessing(videoname):
 
 
     
-def VideoTagging(videoname):
-    #read label text file
-    file = open('label.txt')
-    text=file.read()
-    file.close()
-    lines = text.split('\n')
-    label =[]
-    for line in lines:
-        #emotion AND ./outputs\timestamp...
-        temp = line.split(' ')
-        label.append(temp[0])
-        
-    
-    video = cv2.VideoCapture(videoname +'.avi')
-    
-    Video_width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
-    Video_height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    fps= video.get(cv2.CAP_PROP_FPS)##the number of frames per sec
-    ##fourcc is MJPG,DIVX or XVID 
-    writer = cv2.VideoWriter("tagged_video.avi", cv2.VideoWriter_fourcc(*"MJPG"), fps, (Video_width, Video_height))
-
-    while video.isOpened():
-        
-        
-        
-        #read a fram from video
-        ret, frame = video.read()
-     
-        #if there is no next frame, the loop terminates
-        if not ret: break
-
-        frame= ImageProcessing(frame,'',1,label) # detect a face in an image
-        writer.write(frame)
-
-    writer.release()
-    video.release()
-    cv2.destroyAllWindows()
-        
- 
+##def VideoTagging(videoname):
+##    #read label text file
+##    file = open('label.txt')
+##    text=file.read()
+##    file.close()
+##    lines = text.split('\n')
+##    label =[]
+##    for line in lines:
+##        #emotion AND ./outputs\timestamp...
+##        temp = line.split(' ')
+##        label.append(temp[0])
+##        
+##    
+##    video = cv2.VideoCapture(videoname +'.avi')
+##    
+##    Video_width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+##    Video_height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+##    fps= video.get(cv2.CAP_PROP_FPS)##the number of frames per sec
+##    ##fourcc is MJPG,DIVX or XVID 
+##    writer = cv2.VideoWriter("tagged_video.avi", cv2.VideoWriter_fourcc(*"MJPG"), fps, (Video_width, Video_height))
+##
+##    while video.isOpened():
+##        
+##        
+##        
+##        #read a fram from video
+##        ret, frame = video.read()
+##     
+##        #if there is no next frame, the loop terminates
+##        if not ret: break
+##        
+##        current_frame = video.get(cv2.CAP_PROP_POS_FRAMES)
+##        timestamp = current_frame / fps
+##        #video cropped for only 15 seconds for testing 
+##        if timestamp > 15: break
+##
+##        frame= ImageProcessing(frame,'',1,label) # detect a face in an image
+##        writer.write(frame)
+##
+##    writer.release()
+##    video.release()
+##    cv2.destroyAllWindows()
+##        
+## 
 
 if __name__ == '__main__':
 
-    VideoProcessing('sample3') ##detect face,save the image and return the total number of detected face
-    x_test,y_test = []
-    label,Imgs = CNN_predict(x_test,y_test)
-    VideoTagging('sample3')
+    VideoProcessing('sample2') ##detect face,save the image and return the total number of detected face
+##    x_test,y_test = []
+##    label,Imgs = CNN_predict(x_test,y_test)
+##    VideoTagging('')
     ##FE(imgNo)
         
     
